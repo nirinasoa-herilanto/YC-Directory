22c0d7a98a67ec9334261eabaebfb4b9ed5c9a7f
Nirinasoa Herilanto Randriamananjara <nirinasoa.herilanto@gmail.com>
2024-11-15 14:54:35 +0300

    update[authentication flow]: handling author

--- sanity/queries/author.query.ts
+++ sanity/queries/author.query.ts
@@ -1,7 +1,18 @@
 import { defineQuery } from 'next-sanity';
 
-export const ALL_AUTHORS = defineQuery(`
-    *[_type == "author"]{
+export const ALL_AUTHORS_QUERY = defineQuery(`
+*[_type == "author"]{
+  _id,
+  username,
+  bio,
+  email,
+  image,
+  _createdAt,
+}
+  `);
+
+export const AUTHOR_PROFILE_QUERY = defineQuery(`
+  *[_type == "author" && email == $email][0] {
   _id,
   username,
   bio,
