2d99e07e578c7ea65511203b24e44c0d01518996
Nirinasoa Herilanto Randriamananjara <nirinasoa.herilanto@gmail.com>
2024-11-17 12:07:39 +0300

    feat[page]: add create startup page

---
+++ app/(overview)/startups/create/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@project/auth';
+import { StartupForm } from '@project/components/orgnisms';
+import { HeroPlaceholderType, HeroTemp } from '@project/components/templates';
+import { redirect } from 'next/navigation';
+
+const placeholder = {
+  heading: 'Submit your Startup',
+} as HeroPlaceholderType;
+
+export default async function page() {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect('/');
+  }
+
+  return (
+    <main>
+      <HeroTemp className="!min-h-[250px]" placeholder={placeholder} />
+
+      <section className="my-8">
+        <StartupForm />
+      </section>
+    </main>
+  );
+}
