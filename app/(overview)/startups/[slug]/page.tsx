22209fc3463359be7cde2f3c6e01916da600bf35
Nirinasoa Herilanto Randriamananjara <nirinasoa.herilanto@gmail.com>
2024-11-19 17:15:00 +0300

    fix about working server components
    fix about server components

--- app/(overview)/startups/[slug]/page.tsx
+++ app/(overview)/startups/[slug]/page.tsx
@@ -2,10 +2,12 @@ import { Suspense } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { unstable_after as after } from 'next/server';
 
 import markdownit from 'markdown-it';
 
 import { client } from '@project/sanity/lib/client';
+import { writeClient } from '@project/sanity/lib/write-client';
 import { STARTUP_DETAIL_QUERY } from '@project/sanity/queries';
 
 import { formattedDate } from '@project/utils/app';
@@ -39,9 +41,22 @@ export default async function page({
     tag: formattedDate(new Date(startup._createdAt)),
   } as HeroPlaceholderType;
 
+  after(async () => {
+    await writeClient
+      .withConfig({ useCdn: false })
+      .patch(startup._id)
+      .inc({ views: 1 })
+      .commit();
+  });
+
   return (
     <main>
-      <HeroTemp className="!min-h-[250px]" placeholder={placeholder} showTag />
+      <HeroTemp
+        className="!min-h-[250px]"
+        placeholder={placeholder}
+        showTag
+        showSubHeading
+      />
 
       <section className="w-full h-[500px] my-8 relative px-6 py-10 max-w-screen-lg mx-auto">
         <Image
